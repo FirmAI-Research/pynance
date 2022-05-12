2bb598e655a37bb6ab04a39206cdd5853c84eaad
Michael Sands <michaelesands@gmail.com>
2022-05-12 00:46:40 -0400

    test connect rds

--- db/test_connect.py
+++ db/test_connect.py
@@ -1,15 +1,22 @@
-# IMPORT THE SQALCHEMY LIBRARY's CREATE_ENGINE METHOD
+import sys, os
+import json
+
+# conf
+cwd = os.getcwd()
+parentdir = os.path.dirname(cwd)
+fp = os.path.join(parentdir, 'secrets.json')
+with open(fp, 'r') as f:
+    data = json.load(f)
+
+
 from sqlalchemy import create_engine
+
+user = data['rds_user']
+password = data['rds_password']
+host = 'pynance.ckdvk4iveujs.us-east-1.rds.amazonaws.com'
+port = 5432
+database = 'pynance'
   
-# DEFINE THE DATABASE CREDENTIALS
-user = 'root'
-password = 'password'
-host = '127.0.0.1'
-port = 3306
-database = 'GeeksForGeeks'
-  
-# PYTHON FUNCTION TO CONNECT TO THE MYSQL DATABASE AND
-# RETURN THE SQLACHEMY ENGINE OBJECT
 def get_connection():
     return create_engine(
         url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
@@ -19,10 +26,7 @@ def get_connection():
   
   
 if __name__ == '__main__':
-  
-    try:
-        
-        # GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
+    try:        
         engine = get_connection()
         print(
             f"Connection to the {host} for user {user} created successfully.")
