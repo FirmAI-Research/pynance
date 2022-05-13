b3630faffe237a0f66eeedecce1803f8db897a08
Michael Sands <michaelesands@gmail.com>
2022-05-13 00:47:15 -0400

    rework sector/industry percentiles view

--- pynance/equity_fundamentals/views.py
+++ pynance/equity_fundamentals/views.py
@@ -7,6 +7,8 @@ import json
 import nasdaqdatalink
 from lib.nasdaq import Fundamentals, Metrics, Tickers, Nasdaq
 from lib.calendar import Calendar
+from db.postgres import Postgres
+
 cal = Calendar()
 from dateutil.relativedelta import relativedelta
 import datetime
@@ -88,114 +90,131 @@ def fundamentals(request):
     print(ticker)
 
     if ticker in [None, '', '[None]']:
-        ticker = 'JPM'
+        ticker = 'AMZN'
 
     print(ticker)
 
-    tick = Tickers().get() # FIXME Tickers.get() is bein called twice - Once directly, and once as part of the Fundamentals class
-    ticker_row = tick.loc[tick.ticker == ticker]
-    sector = ticker_row.sector.iloc[0]
-    calendardate = cal.prior_quarter_end()
 
-    if ticker:
-        # Quarterly Fundamental data
-        fun = Fundamentals(calendardate=calendardate)
-        df = fun.fundamentals_by_sector(sector=sector)
-        df = fun.view_sector()
-        print(df.head())
+    engine = Postgres().engine
 
-        # ROE
-        qs, outliers = fun.calculate_box_plot(df, column='roe')
-        qs_json = json.dumps([float(q) for q in qs])
-        outliers_json = json.dumps([float(q) for q in outliers])
+    # Sector
+    sector_percentiles = pd.read_sql_table('Sector_Percentiles_Technology', engine)  # a list of values representing the min, max, median, 1st and 3rd quartile
+    sector_percentiles_values = [float(x) for x in sector_percentiles['pe']]
+    sector_ranks = pd.read_sql_table('Sector_Ranks_Technology', engine) 
+    selected_company_sector_percentile_value = sector_ranks.loc[sector_ranks.ticker == ticker]['pe']    # red dot representing the selected company
+    # Industry
+    industry_percentiles = pd.read_sql_table('Industry_Percentiles_Gold', engine)
+    industry_percentiles_values = [float(x) for x in industry_percentiles['pe']]
+    industry_ranks = pd.read_sql_table('Industry_Ranks_Gold', engine) 
+    selected_company_industry_percentile_value = industry_ranks.loc[industry_ranks.ticker == ticker]['pe']
 
-        # PE
-        qs2, outliers2 = fun.calculate_box_plot(df, column='pe')
-        qs_json2 = json.dumps([float(q) for q in qs2])
-        outliers_json2 = json.dumps([float(q) for q in outliers2])
+    box_values_list =  [sector_percentiles_values] + [industry_percentiles_values]
 
-        try:
-            selected_row_data = df.loc[df['ticker'] == ticker]
-            selected = selected_row_data['roe'].values[0] # 0 preceding selected_pe refers to the column position to plot onto in the chart
-            selected_json = json.dumps([0, float(selected)])
-            selected2 = selected_row_data['pe'].values[0]
-            selected_json2 = json.dumps([0, float(selected2)])
-        except:
-            selected_json = json.dumps([0, 0])
-            selected_json2 = json.dumps([0, 0])
 
-        # Quartiles of the Sector
-        pctile_frame = fun.build_percentiles_frame(df)
-        pctile_frame = pctile_frame.reset_index()
-        print(pctile_frame)
 
-        # Selected Company raw data
-        ticker_data = df.loc[df.ticker == ticker]
-        ticker_data = ticker_data[[x for x in fun.fundamental_cols]]
-        for c in ticker_data.columns:
-            try:
-                ticker_data[c] = ticker_data[c].apply(
-                    lambda x: '{:,.2f}'.format(x))
-            except Exception:
-                pass
-        ticker_data.drop(columns=['calendardate'], inplace=True)
-        ticker_data.reset_index()
 
-        # All companies in the sector
-        all_sector_data = df
-        all_sector_data = all_sector_data[[x for x in fun.fundamental_cols]]
-        for c in ticker_data.columns:
-            if c not in  ['ticker', 'calendardate']:
-                all_sector_data[c] = all_sector_data[c].apply(lambda x: '{:,.2f}'.format(x))
-        all_sector_data.drop(columns=['calendardate'], inplace=True)
-        all_sector_data.reset_index()
-        all_companies_in_sector = all_sector_data
+    # tick = Tickers().get() # FIXME Tickers.get() is bein called twice - Once directly, and once as part of the Fundamentals class
+    # ticker_row = tick.loc[tick.ticker == ticker]
+    # sector = ticker_row.sector.iloc[0]
+    # calendardate = cal.prior_quarter_end()
 
-        # Selected Company percentile rank for each column
-        all_sector_data = df
-        all_sector_data = all_sector_data[[ x for x in fun.fundamental_cols] + ['ticker']]
-        frames = []
-        for c in all_sector_data.columns:
-            if c != 'ticker':
-                frames.append(all_sector_data[c].rank(pct=True, ascending = False))
-        company_pct_rank_data = pd.concat(frames, axis=1)
-        company_pct_rank_data['ticker'] = all_sector_data['ticker'].iloc[:, :1]
-        company_pct_rank_data = company_pct_rank_data.loc[company_pct_rank_data.ticker == ticker]
-        company_pct_rank_data.drop(columns = ['calendardate'], inplace = True)
-        cols = company_pct_rank_data.columns.tolist()
-        cols = cols[-1:] + cols[:-1]
-        company_pct_rank_data = company_pct_rank_data[cols]
-        for c in company_pct_rank_data.columns:
-            if c != 'ticker':
-                company_pct_rank_data[c] = company_pct_rank_data[c].apply(lambda x : '{:,.2f}'.format(x))
-        print(company_pct_rank_data)
+    # if ticker:
+    #     # Quarterly Fundamental data
+    #     fun = Fundamentals(calendardate=calendardate)
+    #     df = fun.fundamentals_by_sector(sector=sector)
+    #     df = fun.view_sector()
+    #     print(df.head())
 
-        context = {
-            'selected_ticker': ticker,
-            'sector': sector,
-            'calendardate':calendardate,
+    #     # ROE
+    #     qs, outliers = fun.calculate_box_plot(df, column='roe')
+    #     qs_json = json.dumps([float(q) for q in qs])
+    #     outliers_json = json.dumps([float(q) for q in outliers])
 
-            'qs_json': qs_json,
-            'selected_pe_json': selected_json,
+    #     # PE
+    #     qs2, outliers2 = fun.calculate_box_plot(df, column='pe')
+    #     qs_json2 = json.dumps([float(q) for q in qs2])
+    #     outliers_json2 = json.dumps([float(q) for q in outliers2])
 
-            'qs_json2': qs_json2,
-            'selected_pe_json2': selected_json2,
+    #     try:
+    #         selected_row_data = df.loc[df['ticker'] == ticker]
+    #         selected = selected_row_data['roe'].values[0] # 0 preceding selected_pe refers to the column position to plot onto in the chart
+    #         selected_json = json.dumps([0, float(selected)])
+    #         selected2 = selected_row_data['pe'].values[0]
+    #         selected_json2 = json.dumps([0, float(selected2)])
+    #     except:
+    #         selected_json = json.dumps([0, 0])
+    #         selected_json2 = json.dumps([0, 0])
 
-            'pctile_frame': pctile_frame,
-            'values': pctile_frame.values.tolist(),
+    #     # Quartiles of the Sector
+    #     pctile_frame = fun.build_percentiles_frame(df)
+    #     pctile_frame = pctile_frame.reset_index()
+    #     print(pctile_frame)
 
-            'ticker_data': ticker_data,
-            'ticker_values': ticker_data.values.tolist(),
+    #     # Selected Company raw data
+    #     ticker_data = df.loc[df.ticker == ticker]
+    #     ticker_data = ticker_data[[x for x in fun.fundamental_cols]]
+    #     for c in ticker_data.columns:
+    #         try:
+    #             ticker_data[c] = ticker_data[c].apply(
+    #                 lambda x: '{:,.2f}'.format(x))
+    #         except Exception:
+    #             pass
+    #     ticker_data.drop(columns=['calendardate'], inplace=True)
+    #     ticker_data.reset_index()
 
-            'all_sector_data': all_companies_in_sector,
-            'sector_values': all_companies_in_sector.values.tolist(),
+    #     # All companies in the sector
+    #     all_sector_data = df
+    #     all_sector_data = all_sector_data[[x for x in fun.fundamental_cols]]
+    #     for c in ticker_data.columns:
+    #         if c not in  ['ticker', 'calendardate']:
+    #             all_sector_data[c] = all_sector_data[c].apply(lambda x: '{:,.2f}'.format(x))
+    #     all_sector_data.drop(columns=['calendardate'], inplace=True)
+    #     all_sector_data.reset_index()
+    #     all_companies_in_sector = all_sector_data
 
-            'company_pct_rank_data': company_pct_rank_data,
-            'company_pct_rank_values': company_pct_rank_data.values.tolist(),
-        }
+    #     # Selected Company percentile rank for each column
+    #     all_sector_data = df
+    #     all_sector_data = all_sector_data[[ x for x in fun.fundamental_cols] + ['ticker']]
+    #     frames = []
+    #     for c in all_sector_data.columns:
+    #         if c != 'ticker':
+    #             frames.append(all_sector_data[c].rank(pct=True, ascending = False))
+    #     company_pct_rank_data = pd.concat(frames, axis=1)
+    #     company_pct_rank_data['ticker'] = all_sector_data['ticker'].iloc[:, :1]
+    #     company_pct_rank_data = company_pct_rank_data.loc[company_pct_rank_data.ticker == ticker]
+    #     company_pct_rank_data.drop(columns = ['calendardate'], inplace = True)
+    #     cols = company_pct_rank_data.columns.tolist()
+    #     cols = cols[-1:] + cols[:-1]
+    #     company_pct_rank_data = company_pct_rank_data[cols]
+    #     for c in company_pct_rank_data.columns:
+    #         if c != 'ticker':
+    #             company_pct_rank_data[c] = company_pct_rank_data[c].apply(lambda x : '{:,.2f}'.format(x))
+    #     print(company_pct_rank_data)
 
-    else:
-        context = {}
+    context = {
+        'selected_ticker': ticker,
+        # 'sector': sector,
+        # 'calendardate':calendardate,
+
+        'data_json': box_values_list,
+        'selected_company_value': [[0,0.2], [0,0.2]],
+
+        # 'qs_json2': qs_json2,
+        # 'selected_pe_json2': selected_json2,
+
+        # 'pctile_frame': pctile_frame,
+        # 'values': pctile_frame.values.tolist(),
+
+        # 'ticker_data': ticker_data,
+        # 'ticker_values': ticker_data.values.tolist(),
+
+        # 'all_sector_data': all_companies_in_sector,
+        # 'sector_values': all_companies_in_sector.values.tolist(),
+
+        # 'company_pct_rank_data': company_pct_rank_data,
+        # 'company_pct_rank_values': company_pct_rank_data.values.tolist(),
+    }
+
 
     return render(request, 'fundamentals.html', context)
 
