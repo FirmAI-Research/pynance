daac75291707f8ad4b33b8c7fcf29b4e5b27fdac
Michael Sands <michaelesands@gmail.com>
2022-05-22 03:42:18 -0400

    dynamic fundamental metric selection for peer comparisons

--- pynance/equity_fundamentals/views.py
+++ pynance/equity_fundamentals/views.py
@@ -86,11 +86,17 @@ def financials(request):
 
 def fundamentals(request):
     ticker = request.POST.get("ticker")
+    selected_metric = request.POST.get("metric")
+    print(selected_metric)
+    if selected_metric in [None, '', '[None]']:
+        selected_metric = 'pe'
 
     if ticker in [None, '', '[None]']:
         ticker = 'AMZN'
     print(ticker)
     print(request.POST)
+
+    engine = Postgres().engine
 
     ndq = Nasdaq()
     ndq.authenticate()
@@ -98,48 +104,41 @@ def fundamentals(request):
     # boxplots
     ticker_data = nasdaqdatalink.get_table('SHARADAR/TICKERS', ticker = ticker)
     industry = ticker_data['industry'].iloc[0]
-    industry_of_selected_ticker = industry.replace(' ','_')
     sector = ticker_data['sector'].iloc[0]
-
+    view_fields = ['netinc', 'roe','roa','roc', 'pe','pb','ps', 'divyield', 'ev/ebitda', 'p/cf', 'opp margin', 'bvps', 'price','interest coverage', 'payoutratio',selected_metric]
+    
     data_of_selected_company = pd.DataFrame(Fundamentals(ticker = ticker).get().iloc[0]).transpose()
-    data_of_selected_company = data_of_selected_company[['calendardate', 'netinc', 'equity', 'debt','assets','pe','pb','ps', 'evebit', 'ebit', 'divyield', 'marketcap', 'ev', 'ebitda', 'fcf','opinc','revenue', 'intexp', 'roe']]
+    data_of_selected_company.rename(columns = {'calendardate': 'date'}, inplace=True)
 
-    calendardate = pd.to_datetime(data_of_selected_company['calendardate'].values[0]).strftime('%Y-%m-%d')
+    calendardate = pd.to_datetime(data_of_selected_company['date'].values[0]).strftime('%Y-%m-%d')
 
-    engine = Postgres().engine
-
-    # boxplots
-    metric_list = ['pe', 'roe']
     box_plot_values = []
     company_values = []
 
-    for metric in metric_list:
+    for metric in [selected_metric]:
         # Sector
         sector_percentiles = pd.read_sql_table('Percentiles_Sector', engine)  # a list of values representing the min, max, median, 1st and 3rd quartile
         sector_percentiles = sector_percentiles.loc[sector_percentiles.sector == sector]
-        sector_percentiles_values = [float(x) for x in sector_percentiles[metric]]
+        sector_percentiles_values = [float(str(x).replace(',','')) for x in sector_percentiles[metric]]
+
         # Industry
         industry_percentiles = pd.read_sql_table(f'Percentiles_Industry', engine)
         industry_percentiles = industry_percentiles.loc[industry_percentiles.industry == industry]
-        industry_percentiles_values = [float(x) for x in industry_percentiles[metric]]
+        industry_percentiles_values = [float(str(x).replace(',','')) for x in industry_percentiles[metric]]
         # Values
         box_plot_values.append( [sector_percentiles_values] + [industry_percentiles_values] )
         company_values.append(   [data_of_selected_company[metric].values[0] ]  + [data_of_selected_company[metric].values[0] ]  )
         print(box_plot_values)
         print(company_values)
 
-    for c in [x for x in data_of_selected_company.columns if x != 'calendardate']:
-        data_of_selected_company[c] = "{:,}".format(float(data_of_selected_company[c]))
 
+    colnames = [x for x in sector_percentiles.columns.tolist() if x not in ['level_0', 'index', 'date','uid']]
 
     # industry percetiles v. metric over time
-    metric_list = ['pe', 'roe']
-
     data = Fundamentals(ticker = ticker).get()
 
     line_chart_json_list = []
-
-    for metric in metric_list:
+    for metric in [selected_metric]:
         metric_data = data[['calendardate', metric]]
         metric_data['calendardate'] = [x.strftime('%Y%m%d') for x in metric_data['calendardate']]
         
@@ -153,9 +152,8 @@ def fundamentals(request):
 
         for df in frames:
             cbind = cbind.merge(df, on ='calendardate')
-
         for c in cbind:
-            cbind[c] = cbind[c].apply(lambda x: float(x))
+            cbind[c] = cbind[c].apply(lambda x: float(str(x).replace(',','')))
 
         cbind.drop('calendardate', axis=1, inplace=True)
         linechart_data = ndq.to_highcharts(cbind)
@@ -164,9 +162,39 @@ def fundamentals(request):
             if i > 0:
                 linechart_data[i]["color"] =  '#D3D3D3'
         line_chart_json_list.append(json.dumps(linechart_data))
-    print(line_chart_json_list[1])
 
 
+    data_of_selected_company = data_of_selected_company[['date'] + view_fields]
+    data_of_selected_company['Peer Group'] = 'N/A'
+    data_of_selected_company['Description'] = 'Values as reported'
+
+    for c in [x for x in data_of_selected_company.columns if x not in  ['date', 'Peer Group', 'Description']]:
+        try:
+            data_of_selected_company[c] = "{:,}".format(float(data_of_selected_company[c]))
+        except:
+            pass
+
+    industry_ranks_of_selected_company = pd.read_sql_table('Ranks_Industry', engine)
+    industry_ranks_of_selected_company = industry_ranks_of_selected_company[['date'] + view_fields].loc[industry_ranks_of_selected_company.ticker == ticker.upper()]
+    industry_ranks_of_selected_company['Peer Group'] = 'Industry'
+    industry_ranks_of_selected_company['Description'] = 'Percentile Rank against Industry Peer Group'
+
+    sector_ranks_of_selected_company = pd.read_sql_table('Ranks_Sector', engine)
+    sector_ranks_of_selected_company = sector_ranks_of_selected_company[['date'] + view_fields].loc[sector_ranks_of_selected_company.ticker == ticker.upper()]
+    sector_ranks_of_selected_company['Peer Group'] = 'Sector'
+    sector_ranks_of_selected_company['Description'] = 'Percentile Rank against Sector Peer Group'
+
+    percentile_values_of_industry= pd.read_sql_table('Percentiles_Industry', engine)
+    percentile_values_of_industry = percentile_values_of_industry[['date'] + view_fields].loc[(percentile_values_of_industry.industry == industry) & (percentile_values_of_industry['index'] =='median')]
+    percentile_values_of_industry['Peer Group'] = 'Industry'
+    percentile_values_of_industry['Description'] = 'Median Percentile Value amoung Industry Peer Group'
+
+    percentile_values_of_sector = pd.read_sql_table('Percentiles_Sector', engine)
+    percentile_values_of_sector = percentile_values_of_sector[['date'] + view_fields].loc[(percentile_values_of_sector.sector == sector) & (percentile_values_of_sector['index'] =='median')]
+    percentile_values_of_sector['Peer Group'] = 'Sector'
+    percentile_values_of_sector['Description'] = 'Median Percentile Value amoung Sector Peer Group'
+
+    company_fundamentals = pd.concat([data_of_selected_company, percentile_values_of_industry, percentile_values_of_sector, industry_ranks_of_selected_company, sector_ranks_of_selected_company, ], axis=0, ignore_index=True)
 
 
     context = {
@@ -175,18 +203,21 @@ def fundamentals(request):
         'sector': sector,
         'industry':industry,
         'calendardate':calendardate,
+        'colnames':colnames,
         # boxplots
         'box_plot_values_1': box_plot_values[0],
         'selected_company_values_1': company_values[0],
-        'box_plot_values_2': box_plot_values[1],
-        'selected_company_values_2': company_values[1],
+        # 'box_plot_values_2': box_plot_values[1],
+        # 'selected_company_values_2': company_values[1],
+
+
         # industry percentiles over time
         'line_chart_values_1':line_chart_json_list[0],
-        'line_chart_values_2':line_chart_json_list[1],
+        # 'line_chart_values_2':line_chart_json_list[1],
 
 
-        'company_fundamentals':data_of_selected_company,
-        'values':data_of_selected_company.values.tolist(),
+        'company_fundamentals':company_fundamentals,
+        'values':company_fundamentals.values.tolist(),
 
     }
 
