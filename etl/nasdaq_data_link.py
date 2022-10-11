f6dc450b2c105892c3ee8352b7d03ccb0f5c19c5
Michael Sands <michaelesands@gmail.com>
2022-10-10 23:11:42 -0400

    start etl fix for nasdaq class

--- etl/nasdaq_data_link.py
+++ etl/nasdaq_data_link.py
@@ -2,7 +2,24 @@
   ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
   │                                                      Nasdaq Data Link                                              │
   └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
- """
+"""
+
+import re
+import nasdaqdatalink
+import json
+import sys, os
+import pandas as pd
+import numpy as np
+import requests
+
+proj_root = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(proj_root)
+
+from calendar_dates import Calendar
+cal = Calendar()
 
 
- 
+class NasdaqDataLink:
+
+  def init(self):
+    pass
