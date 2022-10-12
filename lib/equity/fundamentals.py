25cfbbc985134b6ec6701a719375649bb4f8daea
Michael Sands <michaelesands@gmail.com>
2022-10-11 23:02:13 -0400

    create Fundamentals object

--- lib/equity/fundamentals.py
+++ lib/equity/fundamentals.py
@@ -1,7 +1,8 @@
-# Sector Percentile Ranks; Financial Statements; DCF
+# Fundamentals; Sector Percentile Ranks;  DCF; Financial Statements;
 import sys, os
 import nasdaqdatalink
 import pandas as pd 
+from scipy.stats.mstats import gmean
 
 proj_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(proj_root)
@@ -13,24 +14,94 @@ cal = Calendar()
 import nasdaq_data_link as nasdaq
 from nasdaq_data_link import Sharadar
 
-from etl.fundamentals_etl import FundamentalsETL
 
-from scipy.stats.mstats import gmean
+
+class Fundamentals:
+    ''' A class which accepts a Ticker as a parameter and returns equity fundamental's.
+    '''
+    # 1. query fundamental data for all companies as of the previous quarter end date
+    # 2. calculate desired 'custom' metrics on fundamentals
+    # 3. populate sql table for all companies, with custom metrics, other metrics to be used for ranks, and sector/industry. Table name = EquityFundamentalMetrics
+
+    id_columns = [
+        'ticker', 'calendardate'
+
+    ]
+
+    dcf_columns = id_columns + [
+        'revenue', 'ebitda', 'ebit', 'netinc',  'opinc', 'taxexp', 'ebt'
+        
+    ]
+
+    peer_compare_columns = id_columns + [ 
+        'bvps', 'currentratio', 'de', 'dps', 'divyield', 'eps', 'evebit', 'evebitda', 'fcfps', 'grossmargin', 'netmargin', 'pb', 'pe', 'price', 'roa', 'roe', 'roic', 'ros', 
+        'roc', 'fcfmargin', 'p/cf', 'oppmargin', 'interestcoverage', 'payoutratio', 'tax rate', 'retention ratio', 'expected netinc growth', 'expected roe growth', 
+        'equity reinvestment rate','expected ebit growth'
+    ]
+
+
+    def __init__(self, ticker = None):
+
+        nasdaq.Nasdaq()
+        
+        self.df = nasdaqdatalink.get_table(Sharadar.FUNDAMENTALS.value, dimension="MRQ", ticker = ticker,  paginate=True) # All MRQ periods; One Ticker
+        # self.df = nasdaqdatalink.get_table(Sharadar.FUNDAMENTALS.value, dimension="MRQ",   paginate=True) # All MRQ periods; All Tickers
+
+        self.custom_metric_calcs()
+
+        self.df = self.df[::-1]
+
+
+    def custom_metric_calcs(self):
+        ''' additional metrics calculated based on raw fundamental data
+        
+        damodaran pg. 178
+        '''
+
+        df = self.df
+
+        df['roc'] = df['netinc'] / (df['equity'] + df['debt'])
+        df['fcfmargin'] = df['fcf'] / df['revenue']
+        df['p/cf'] = df['marketcap'] / df['fcf']
+        df['oppmargin'] = df['opinc'] / df['revenue']
+        df['interestcoverage'] = df['ebit'] / df['intexp']
+        df['payoutratio'] = df['dps'] / df['eps']
+        df['tax rate'] = df['taxexp'] / df['ebt']
+        df['retention ratio'] = (df['retearn']  / df['netinc']) / 100
+        df['expected netinc growth'] = df['retention ratio'] * df['roe']
+        df['expected roe growth'] = df['ebit'] *  df['tax rate'] / (df['equity'] + df['debt'])
+        df['equity reinvestment rate'] =   df['expected netinc growth'] /  df['roe']  
+        df['expected ebit growth'] = df['equity reinvestment rate'] * df['roc']
+        df['sales to capital ratio'] = '' # reinvestment rate
+
+        self.df = df
+
+
 
 
 class DiscountedCashFlow():
 
+    n_historical_periods = 5
 
-    def __init__(self, df):
-        '''
-        :param: df : time series of equity fundamentals for an individual company
-        '''
-        n_historical_periods = 5
+    n_future_periods = 4
+    
 
-        self.df = df.iloc[-n_historical_periods:]
+    def __init__(self, fun_obj):
+        ''' Constructs a dcf using fundamental data passed in a ~Fundamentals~ object. Raw data is stored in Fundamentals().df
+
+        :param: fun_obj : An object of type ~Fundamentals~
+        '''
+        df = fun_obj.df[fun_obj.dcf_columns].set_index('calendardate').drop('ticker', axis=1)
+
+        self.fundamentals = df.iloc[-self.n_historical_periods:]
+
+        print(self.fundamentals)
         
         self.build_forecasting_table()
-     
+
+        print(self.forecast)
+
+
 
     def build_forecasting_table(self):
 
@@ -38,12 +109,34 @@ class DiscountedCashFlow():
         
         self.cagr(col = 'revenue')
         self.arithmetic_average(col = 'revenue')
-        self.cagr(col = 'oppmargin')
-        self.arithmetic_average(col = 'oppmargin')
+        
+        self.cagr(col = 'ebitda')
+        self.arithmetic_average(col = 'ebitda')        
+        
+        self.cagr(col = 'ebit')
+        self.arithmetic_average(col = 'ebit')
+        
+        self.cagr(col = 'netinc')
+        self.arithmetic_average(col = 'netinc')   
+        
         self.cagr(col = 'opinc')
         self.arithmetic_average(col = 'opinc')   
 
-        print(self.forecast)
+
+    def arithmetic_average(self, col):
+        label = col + '_aagr'
+        self.forecast[label] = self.fundamentals[col].pct_change().dropna().mean()
+
+
+    def cagr(self, col):
+        label = col + '_cagr'
+
+        start_value, end_value = self.fundamentals[col][0],  self.fundamentals[col][-1]
+        num_periods = len(self.fundamentals[col])
+        
+        self.forecast[label]  = [(end_value / start_value) ** (1 / (num_periods - 1)) - 1 for x in self.fundamentals[col]]
+
+
 
 
     def build_assumptions_table(self):
@@ -54,16 +147,17 @@ class DiscountedCashFlow():
         pass
 
 
-    def arithmetic_average(self, col):
-        label = col + '_aagr'
-        self.forecast[label] = self.df[col].pct_change().dropna().mean()
 
 
-    def cagr(self, col):
-        label = col + '_cagr'
+class RanksETL:
 
-        series = self.df[col]
-        start_value, end_value = series[0],  series[-1]
-        num_periods = len(series)
-        
-        self.forecast[label]  = [(end_value / start_value) ** (1 / (num_periods - 1)) - 1 for x in self.df[col]]
+
+    def __init__(self):
+        pass
+
+    def query(self):
+        pass
+
+
+
+
--- main.py
+++ main.py
@@ -15,9 +15,82 @@ from nasdaq_data_link import Sharadar
   │ Call DCF                                                                 │
   └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
  """
-from etl.fundamentals_etl import FundamentalsETL
+from lib.equity.fundamentals import Fundamentals
 from lib.equity.fundamentals import DiscountedCashFlow
 
-fun = FundamentalsETL()
-df = fun.df[fun.dcf_columns].set_index('calendardate').drop('ticker', axis=1)
-dcf = DiscountedCashFlow(df)
+fun = Fundamentals( ticker = 'XOM' )
+dcf = DiscountedCashFlow(fun)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import sys, os
+# import nasdaqdatalink
+# import pandas as pd
+
+# proj_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print(proj_root)
+# sys.path.append(proj_root)
+
+# from calendar_dates import Calendar
+# cal = Calendar()
+
+# import nasdaq_data_link as nasdaq
+# from nasdaq_data_link import Sharadar
+
+# from etl.fundamentals_etl import FundamentalsETL
+
+# pd.options.display.float_format = '{:,.2f}'.format
+
+
+# """ 
+#   ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+#   │                                  Retreive data from NasdaqDataLink                                                 │
+#   └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+# """
+# def get_nasdaq_data():
+
+#   query = nasdaq.Nasdaq()
+#   df = nasdaqdatalink.get_table(Sharadar.METRICS.value, ticker = ['AAPL','AMZN'], date = cal.previous_quarter_end())
+#   print(df)
+
+# # get_nasdaq_data()
+
+
+
+
+
+
+# """ 
+#   ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+#   │                                      Equity Fundamentals Ranks                                                     |
+#   └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+#  """
+#  # use fundamentals.Ranks  object to calculate ranks and store in database
+# def etl_equity_fundamental_ranks():
+#   fun = FundamentalsETL()
+#   fun.custom_calculations()
+
+# etl_equity_fundamental_ranks()
